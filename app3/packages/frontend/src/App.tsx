751a03caf3d1e6ccb1ec1b485ad8a39fe759179f
devpilotlab <developer@devpilotlab.dev>
2025-06-01 18:07:52 +0900

    todoリスト

--- app3/packages/frontend/src/App.tsx
+++ app3/packages/frontend/src/App.tsx
@@ -3,31 +3,50 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+interface Todo {
+  id: number;
+  title: string;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, title: 'Learn React' },
+    { id: 2, title: 'Learn TypeScript' },
+    { id: 3, title: 'Build a Todo App' }
+  ]);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const titleInput = e.currentTarget.querySelector('input[name="title"]');
+    const newTodoTitle = (titleInput as HTMLInputElement).value;
+    if (newTodoTitle.trim() === '') return;
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      title: newTodoTitle
+    }
+    setTodos([...todos, newTodo]);
+  };
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="title" placeholder="Add a new todo" />
+          <button type="submit">Add Todo</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div>
+        {todos.map(todo => {
+          return (
+            <div>
+
+              <span>{todo.id}</span>
+              <span>{todo.title}</span></div>
+            )
+          })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
